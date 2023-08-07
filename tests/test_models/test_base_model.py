098e74a725a91d356e5dc43645ac84bdcdadd827
Blxee <aminedev57@gmail.com>
2023-08-07 11:24:34 +0100

    Created: test file for BaseModel class

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    
+    def test_save(self):
+        pass
+    
+    def test_to_dict(self):
+        pass
+    
+    def test_str(self):
+        pass
