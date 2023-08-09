910b09d0aa78295885f6ecb190983211fe8e7578
Nawaritta <Nor.benhaddou@gmail.com>
2023-08-09 12:10:04 +0000

    task 5 without reload

---
+++ models/engine/file_storage.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" This module defines the class Filestorage """
+import json
+from os import path
+
+
+class FileStorage():
+    """ This class serializes instances to a JSON file and deserializes JSON file to instances
+
+    Private class attributes:
+    __file_path: string - path to the JSON file (ex: file.json)
+    __objects: dictionary - empty but will store all objects by <class name>.id
+
+    Public instance methods:
+    all(self): returns the dictionary __objects
+    new(self, obj): sets in __objects the obj with key <obj class name>.id
+    save(self): serializes __objects to the JSON file (path: __file_path)
+    reload(self): deserializes the JSON file to __objects.
+    """
+
+    __file_path = 'creatd_instances.json'
+    __objects = {}
+
+    def __init__(self):
+        pass
+
+    def all(self):
+        """
+        Returns:
+
+        dict: A dictionary containing all serialized objects.
+
+        """
+        return self.__objects
+
+
+
+    def new(self, obj):
+        """
+        Adds a new object to the dictionary of serialized objects.
+
+        Args:
+            obj: The object to be added to the dictionary.
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+
+    def save(self):
+        """
+        Serializes and saves all objects in the dictionary to the JSON file.
+        (path: __file_path)
+
+        """
+        serialized = {}
+        for key, value in self.__objects.items():
+            serialized[key] = obj.to_dict()
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(serialized, file)
+
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects, if the JSON
+        file exists, otherwise nothing happens)
+        """
