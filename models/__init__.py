723c99201604b74e411ff0e8c699af4b5738c423
Nawaritta <Nor.benhaddou@gmail.com>
2023-08-09 13:24:08 +0000

    reload function _ circular call!

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@
 """
  Create the storage instance & Load serialized objects from the JSON file
 """
-from models.engine.file_storage import FileStorage
+from .engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -13,16 +13,28 @@ class BaseModel:
         updated_at (datetime): update time
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) == 0:
+            from . import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            self.__dict__ = kwargs
+            del self.__dict__['__class__']
+            self.__dict__['created_at'] = \
+                datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.__dict__['updated_at'] = \
+                datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
 
     def save(self):
         """
         Updates the ``updated_at`` attributes to the current time
         """
+        from . import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
@@ -30,11 +42,14 @@ class BaseModel:
         Returns:
             a dictionary representing all the instance attributes
         """
-        dict = self.__dict__
+        dict = self.__dict__.copy()
         dict['__class__'] = self.__class__.__name__
         dict['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         dict['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         return dict
 
     def __str__(self):
+        """
+        Returns the string representation of BaseModel.
+        """
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
