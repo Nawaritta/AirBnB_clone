b7ea4724d1b65001b73ad8729b016e1acec77ee3
Nawaritta <Nor.benhaddou@gmail.com>
2023-08-12 00:53:02 +0000

    need to check do_methods first

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """Entry point of the command interperter"""
 import cmd
 from re import compile
+import re
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,8 +13,20 @@ from models.review import Review
 from models import storage
 
 
+
 def tokenize(line, _pattern=compile(r'("[^"]*"|\s*\S+\s*)')):
-    return list(map(lambda s: s.strip('" '), _pattern.findall(line)))
+    dot = False
+    if "." in line:
+        dot = True
+        line = line.replace(".", " ", 1)
+        line = line.replace("(", "", 1).replace(")","",1)
+
+    tokenized_list = list(map(lambda s: s.strip('" '), _pattern.findall(line)))
+
+    if dot and len(tokenized_list) >= 2:
+        tokenized_list[0], tokenized_list[1] = tokenized_list[1], tokenized_list[0]
+
+    return tokenized_list
 
 
 class HBNBCommand(cmd.Cmd):
@@ -35,7 +48,9 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Creates and saves a new instance"""
+        print(arg)
         args = tokenize(arg)
+        print(args)
         if len(args) == 0:
             print('** class name missing **')
             return
@@ -133,7 +148,45 @@ class HBNBCommand(cmd.Cmd):
         attr_value = class_type(args[3])
         setattr(instance, attr_name, attr_value)
         instance.save()
+        '''
+    def instance_dot_cmd(self, line):
+        """Check for <class>.<cmd>(<args>) syntax and execute corresponding
+        method to execute instance
+        """
+        args = tokenize(line)
+        if args[0] == 'all':
+            self.do_all(' '.join(args[1:]))
+        elif args[0] == 'create':
+            self.do_create(' '.join(args[1:]))
+        elif args[0] == 'count':
+            self.do_count(' '.join(args[1:]))
+        elif args[0] == 'show':
+            self.do_show(' '.join(args[1:]))
+        elif args[0] == 'destroy':
+            self.do_destroy(' '.join(args[1:]))
+        elif args[0] == 'update':
+            self.do_updae(' '.join(args[1:]))
+        '''
 
+    def instance_dot_cmd(self, line):
+        """
+        Check for <class>.<cmd>(<args>) syntax and execute corresponding
+        method to execute instance
+        """
+        args = tokenize(line)
+        cmd_list = { 'all': self.do_all, 'create': self.do_create,
+                     'show': self.do_show, 'destroy': self.do_destroy,
+                     'update': self.do_update
+        }
+        if args[0] in cmd_list:
+            cmd_func = cmd_list[args[0]]
+            cmd_func(' '.join(args[1:]))
+
+
+    def onecmd(self, line):
+        """This method is called when a line is read from input.
+        It directly calls the instance_dot_cmd function with the tokenized input.
+        """
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
