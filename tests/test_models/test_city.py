ef00377a92b2c9c69af71b4aac2a408f9cd802c8
Blxee <aminedev57@gmail.com>
2023-08-12 11:25:33 +0100

    Created: placeholders for tests

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,13 @@
+import unittest
+from models.city import City
+"""Module for the City class tests"""
+
+
+class TestCity(unittest.TestCase):
+    """Main class for testing City"""
+
+    def test_init(self):
+        """
+        Tests the attributes of a City instance as well as __init__()
+        """
+        pass
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,13 @@
+import unittest
+from models.user import User
+"""Module for the User class tests"""
+
+
+class TestUser(unittest.TestCase):
+    """Main class for testing User"""
+
+    def test_init(self):
+        """
+        Tests the attributes of a BaseModel instance as well as __init__()
+        """
+        pass
