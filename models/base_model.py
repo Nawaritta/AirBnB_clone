3d94c8b7d34481ae09db2db10cf2e993f054eebf
Blxee <aminedev57@gmail.com>
2023-08-14 01:30:20 +0100

    Changed: base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -14,15 +14,15 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        if len(kwargs) == 0:
+        if kwargs is None or len(kwargs) == 0:
             from . import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            self.__dict__ = kwargs.copy()
-            del self.__dict__['__class__']
+            del kwargs['__class__']
+            self.__dict__ = kwargs
             self.__dict__['created_at'] = \
                 datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             self.__dict__['updated_at'] = \
