b3b979891ff7f5090ecc3f0b282c30e36c9ad00a
Blxee <aminedev57@gmail.com>
2023-08-14 01:14:35 +0100

    Changed: kwargs is none in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        if kwargs is None or len(kwargs) == 0:
+        if len(kwargs) == 0:
             from . import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
