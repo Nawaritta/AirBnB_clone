ee90e3d4d9691cc813033d0c7fcd6693d7daa448
Blxee <aminedev57@gmail.com>
2023-08-14 09:17:55 +0100

    Fixed: failing tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -116,7 +116,7 @@ class TestFileStorage(unittest.TestCase):
         storage = FileStorage()
         with self.assertRaises(TypeError) as err:
             storage.all("exess")
-        self.assertEqual(str(err.exception), 'FileStorage.all() takes 1'
+        self.assertEqual(str(err.exception), 'all() takes 1'
                          + ' positional argument but 2 were given')
         self.assertEqual(storage.all(), storage._FileStorage__objects)
 
@@ -124,18 +124,18 @@ class TestFileStorage(unittest.TestCase):
         storage = FileStorage()
         with self.assertRaises(TypeError) as err:
             storage.new()
-        self.assertEqual(str(err.exception), 'FileStorage.new() missing 1'
+        self.assertEqual(str(err.exception), 'new() missing 1'
                          + " required positional argument: 'obj'")
         with self.assertRaises(TypeError) as err:
             storage.new("obj", "exess")
-        self.assertEqual(str(err.exception), 'FileStorage.new() takes 2'
+        self.assertEqual(str(err.exception), 'new() takes 2'
                          + ' positional arguments but 3 were given')
 
     def test_save(self):
         storage = FileStorage()
         with self.assertRaises(TypeError) as err:
             storage.save("exess")
-        self.assertEqual(str(err.exception), 'FileStorage.save() takes 1'
+        self.assertEqual(str(err.exception), 'save() takes 1'
                          + ' positional argument but 2 were given')
         storage.save()
         file_name = storage._FileStorage__file_path
@@ -145,5 +145,5 @@ class TestFileStorage(unittest.TestCase):
         storage = FileStorage()
         with self.assertRaises(TypeError) as err:
             storage.reload("exess")
-        self.assertEqual(str(err.exception), 'FileStorage.reload() takes 1'
+        self.assertEqual(str(err.exception), 'reload() takes 1'
                          + ' positional argument but 2 were given')
